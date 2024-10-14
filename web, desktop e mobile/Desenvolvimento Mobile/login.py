321e881577f03936ad260b6ac761d820649a0cfd
rafaelreis8000 <priv.rafa.reis@gmail.com>
2024-10-14 13:44:20 -0300

    .

--- web, desktop e mobile/Desenvolvimento Mobile/login.py
+++ web, desktop e mobile/Desenvolvimento Mobile/login.py
@@ -1,12 +1,14 @@
 import flet as ft
 
 # Classe da Aplicação de Login
-class LoginApp:
+class Login:
     def __init__(self, page: ft.Page):
         # Inicialização da Página
         self.page = page
         self.page.title = "Login App"
         self.page.vertical_alignment = ft.MainAxisAlignment.CENTER
+        self.page.window_width =  300
+        self.page.bgimage("C:\Users\master\Documents\GitHub\Projeto_Cidade_verde\web, desktop e mobile\Ícones e vetores\logo\logo\logo")
 
         # Campos de login
         self.username = ft.TextField(label="Usuário", width=300)
@@ -35,7 +37,7 @@ class LoginApp:
 
     # Função que realiza o login
     def login(self, e):
-        # Exemplo simples de verificação de login
+        # Verificação de login
         if self.username.value == "admin" and self.password.value == "123":
             self.status_message.value = "Login realizado com sucesso!"
             self.status_message.color = ft.colors.GREEN
@@ -48,7 +50,7 @@ class LoginApp:
 
 # Função principal para executar o aplicativo
 def main(page: ft.Page):
-    app = LoginApp(page)
+    app = Login(page)
 
 # Execução da aplicação Flet
 ft.app(target=main)
