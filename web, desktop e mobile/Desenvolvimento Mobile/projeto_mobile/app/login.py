55809cebc1a70a4fa2ef91df269023b6bb21f71f
rafaelreis8000 <priv.rafa.reis@gmail.com>
2024-10-16 00:53:31 -0300

    .

--- web, desktop e mobile/Desenvolvimento Mobile/projeto_mobile/app/login.py
+++ web, desktop e mobile/Desenvolvimento Mobile/projeto_mobile/app/login.py
@@ -2,12 +2,22 @@ import flet as ft
 
 def login(page: ft.Page):
     return ft.Container(
+        expand= True,
         bgcolor= "#1D3331",
         alignment= ft.alignment.center,
         content= ft.Column(
             controls= [
-                ft.Text("FAÇA SEU LOGIN: ", size=40),
-                ft.TextField(label="Insira seu E-mail: ")
+
+                ft.Text("FAÇA SEU LOGIN: "),
+                ft.TextField(label="Insira seu E-mail: "),
+                ft.TextField(label= "Insira sua senha: "),
+                
+                    #um container somente para o botão login, com alinhamento diferente
+                    ft.Container(
+                        alignment= ft.alignment.center_right,
+                        content= ft.ElevatedButton("LOGIN")
+                    )
+
             ]
         )
     )
