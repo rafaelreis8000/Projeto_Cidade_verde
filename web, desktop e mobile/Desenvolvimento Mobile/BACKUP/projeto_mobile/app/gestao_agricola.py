4cda80a532965d142fa1db2ae2f3b53291a72952
rafaelreis8000 <priv.rafa.reis@gmail.com>
2024-10-23 18:29:20 -0300

    .

--- web, desktop e mobile/Desenvolvimento Mobile/BACKUP/projeto_mobile/app/gestao_agricola.py
+++
@@ -1,6 +0,0 @@
-import flet as ft
-
-def gestao_agricola(page: ft.Page):
-    return ft.Container(
-        ft.Text ("GESTÃO AGRÍCOLA", size= 40),
-    )
--- web, desktop e mobile/Desenvolvimento Mobile/BACKUP/projeto_mobile/app/gestao_insumos.py
+++
@@ -1,6 +0,0 @@
-import flet as ft
-
-def gestao_insumos(page: ft.Page):
-    return ft.Container(
-        ft.Text ("GESTÃO INSUMOS", size= 40),
-    )
